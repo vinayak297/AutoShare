36233f554150eea1de338e9571631263237e800d
Vinayak <257292516+vinayak297@users.noreply.github.com>
2026-03-14 09:26:39 +0530

    Firebase connected

---
+++ frontend/src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDROdxFiaBDsD8UcTVIQUMDIqXt4tXWG_Q",
+  authDomain: "autoshare-cfa0a.firebaseapp.com",
+  projectId: "autoshare-cfa0a",
+  storageBucket: "autoshare-cfa0a.firebasestorage.app",
+  messagingSenderId: "230895451243",
+  appId: "1:230895451243:web:261bb3db622ddef7abc52e"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
